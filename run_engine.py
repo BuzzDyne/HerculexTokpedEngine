c5e53fcd37326cd0d050eb7774d9d4de98049ac6
BuzzDyne <buzzdyne@gmail.com>
2023-11-13 00:19:35 +0700

    Deploy

--- run_engine.py
+++ run_engine.py
@@ -1,27 +1,7 @@
-import sys
-
-from app import create, update
-
-"""
-    Must be called with either one of two params
-        --update    = to update data
-        --create    = to retrieve new listing data
-"""
-
-
-def checkArgs():
-    argsCount = len(sys.argv)
-
-    if argsCount != 2:
-        print("Only one argument required!")
-    else:
-        if sys.argv[1] == "--update":
-            update()
-        elif sys.argv[1] == "--create":
-            create()
-        else:
-            print("Wrong argument!\n('--update' or '--create'")
+from app import App
 
 
 if __name__ == "__main__":
-    checkArgs()
+    a = App()
+    a.syncTokpedExsOrderData()
+    a.syncTokpedNewOrderData()
